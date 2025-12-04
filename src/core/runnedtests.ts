1a15c4907dd8c094f4629a0007cab397bbab5204
Lukas <r.yusifli@yahoo.com>
2025-12-04 20:41:54 +0100

    Exported the colors

--- src/core/runnedtests.ts
+++ src/core/runnedtests.ts
@@ -2,9 +2,10 @@ import { RunnedTests } from "./globals";
 import { passed } from "../utils/colors";
 import "../index";
 
+
 for (const runnedTest of RunnedTests) {
     if (runnedTest.Status === "Passed") {
-        passed(`${runnedTest.description} \n ${runnedTest.duration}`)
+        passed(`${runnedTest.description} \n ${runnedTest.duration}ms`)
     }
 
     if (runnedTest.Status === "Failed") {
